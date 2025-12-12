8874fa22ec99dc5e64a01a3ea89cd9a010d610a6
Treader7 <AymanWael7@protonmail.com>
2025-12-12 17:48:01 +0200

    Second commit
    created error file and added comments for constants file and other values

--- Constants.py
+++ Constants.py
@@ -1,4 +1,8 @@
-BMP_signature= b'BM'
-Magic_number=0xAAFF7799
-Supported_Bit_Depths= [24, 32]
+BMP_signature= b'BM' # BMP files start signature (first 2 bytes of the file)
+Magic_number=0xAAFF7799 # unique identfier or signature
+Supported_Bit_Depths= [24, 32] # supported bit depths for BMP files 24 for RBG and 32 for RGBA
 Max_Message_Size= 1000000  # 1 MB
+BMP_header_size= 54 # BMP header size so we can tell where pixel data starts
+Header_size_bits = 96 # size of steganography header in bits
+Supported_Extensions= ['.bmp'] # Supported file extensions
+Default_Output_Extension= ['.bmp'] # default output file extension
